9d448da3c5f37e07b82a45e66cbf4ffe0be20f62
Jonathan <jonathan.mfm@gmail.com>
2026-01-26 13:36:54 +0100

    feat: créer la page Home
    - Ajouter le composant Home
    - Créer la section "Mes projets"
    - Préparer l'affichage des projets via ProjectGallery

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,21 @@
+import ProjectGallery from "../components/ProjectGallery";
+
+function Home() {
+  return (
+    <>
+      {/* Section Mes projets */}
+      <section
+        id="projects"
+        className="bg-gray-50 py-8 lg:py-12 lg:flex lg:items-center"
+      >
+        {/* Container */}
+        <div className="max-w-6xl mx-auto px-6 w-full">
+          <h2>Mes projets</h2>
+          <ProjectGallery />
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Home;
