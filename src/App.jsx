182a40632936e8a0672fccd0f6c5e65e2396ead3
Jonathan <jonathan.mfm@gmail.com>
2026-01-26 13:38:02 +0100

    feat: intégrer Home dans App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import Layout from "./components/Layout";
+import Home from "./Pages/Home";
 
 function App() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 }
 
 export default App;
