badf31c63df8151406b12ada31e137a45ca80aca
Jonathan <jonathan.mfm@gmail.com>
2026-01-26 13:32:16 +0100

    feat: créer composant ProjectGallery avec tri des projets

---
+++ src/components/ProjectGallery.jsx
@@ -0,0 +1,18 @@
+import ProjectCard from "./ProjectCard";
+import projects from "../data/projects";
+
+function ProjectGallery() {
+  const sortedProjects = [...projects].sort(
+    (a, b) => new Date(b.date) - new Date(a.date),
+  );
+
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-7 items-stretch">
+      {sortedProjects.map((project) => (
+        <ProjectCard key={project.id} project={project} />
+      ))}
+    </div>
+  );
+}
+
+export default ProjectGallery;
