1e4ceaa9b7bc44baeda0f6c4ed2d236deed8722c
Jonathan <jonathan.mfm@gmail.com>
2026-01-29 10:10:23 +0100

    refactor: adapter le layout pour utiliser Outlet de react-router

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import { Outlet } from "react-router";
 
-function Layout({ children }) {
+function Layout() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <main className="w-full">{children}</main>
+      <main className="flex-1 w-full">
+        <Outlet />
+      </main>
       <Footer />
-    </>
+    </div>
   );
 }
 
