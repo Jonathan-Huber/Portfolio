dd2e09899204ecc99b2249e407e06b6de2715f3b
Jonathan <jonathan.mfm@gmail.com>
2026-01-29 10:06:10 +0100

    refactor: améliorer la mise en page et la responsivité du hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,20 @@ import Button from "./Button";
 // Hero.jsx
 function Hero() {
   return (
-    <section className="bg-cyan-50 py-8 lg:py-26 lg:flex lg:items-center">
+    <section className="min-h-screen bg-cyan-50 py-8 flex items-center">
       {/* Container */}
       <div className="container mx-auto px-6 xl:px-48 pt-16">
-        <div className="flex flex-col-reverse md:flex-row items-center justify-between text-center md:text-left">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-between text-center lg:text-left">
           {/* Texte */}
-          <div className="md:w-1/2">
-            <h1>Jonathan Huber</h1>
-            <div className="w-12 h-1 bg-cyan-600 my-2 mx-auto md:mx-0 rounded-full"></div>
+          <div className="lg:w-1/2">
+            <h1 className="lg:text-7xl">Jonathan Huber</h1>
+            <div className="w-12 h-1 bg-cyan-600 my-2 mx-auto lg:mx-0 rounded-full"></div>
             <p className="subtitle">Intégrateur web / Front-end</p>
             <p>
               Passionné par le front-end, je crée des sites modernes et
               performants.
             </p>
-            <div className="flex flex-col sm:flex-row gap-4 mt-6 px-10 md:px-0 justify-center md:justify-start">
+            <div className="flex flex-col sm:flex-row gap-4 mt-6 px-10 md:px-0 justify-center lg:justify-start">
               <Button href="#projects" variant="primary">
                 Voir mes projets
               </Button>
@@ -27,13 +27,13 @@ function Hero() {
           </div>
 
           {/* Avatar / Illustration */}
-          <div className="md:w-1/2 mb-6 md:mb-0 flex justify-center md:justify-end">
+          <div className="md:w-1/2 mb-6 lg:mb-0 flex justify-center lg:justify-end">
             <img
               src="/images/avatar-480.webp"
               srcSet="/images/avatar-320.webp 320w, /images/avatar-480.webp 480w, /images/avatar-750.webp 750w"
               sizes="(min-width: 768px) 288px, 256px"
               alt="Illustration Jonathan Huber"
-              className="w-64 h-64 md:w-72 md:h-72 rounded-full object-cover ring-4 ring-black/20"
+              className="w-64 h-64 sm:w-72 sm:h-72 lg:w-80 lg:h-80 rounded-full object-cover ring-4 ring-black/20"
               fetchPriority="high"
             />
           </div>
