713acac8a96b8fe476b65e8d9bf3a6b5c833aebf
Jonathan <jonathan.mfm@gmail.com>
2026-01-30 09:27:02 +0100

    feat: créer le composant skillsCloud et l'intégrer à la page Home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,8 @@
+import About from "../components/About";
+import ContactForm from "../components/Contact";
+import Journey from "../components/Journey";
 import ProjectGallery from "../components/ProjectGallery";
+import SkillsCloud from "../components/SkillsCloud";
 
 function Home() {
   return (
@@ -6,12 +10,23 @@ function Home() {
       {/* Section Mes projets */}
       <section
         id="projects"
-        className="bg-gray-50 py-8 lg:py-12 lg:flex lg:items-center"
+        className="bg-gray-50 py-8 lg:py-20 lg:flex lg:items-center"
       >
         {/* Container */}
         <div className="container mx-auto px-6">
           <h2>Mes projets</h2>
           <ProjectGallery />
+        </div>
+      </section>
+
+      {/* Section Mes compétences */}
+      <section
+        id="skills"
+        className="bg-cyan-50 py-8 lg:py-20 lg:flex lg:items-center"
+      >
+        <div className="container text-center mx-auto px-6 lg:px-75 xl:px-100 2xl:px-125">
+          <h2>Compétences</h2>
+          <SkillsCloud />
         </div>
       </section>
     </>
---
+++ src/components/SkillsCloud.jsx
@@ -0,0 +1,103 @@
+const skillsData = [
+  {
+    name: "JavaScript",
+    level: 4,
+    color: "bg-yellow-400 text-gray-800",
+    rotate: "rotate-0",
+  },
+  {
+    name: "React",
+    level: 4,
+    color: "bg-cyan-700 text-white",
+    rotate: "-rotate-2",
+  },
+  {
+    name: "Redux",
+    level: 3,
+    color: "bg-cyan-400 text-gray-900",
+    rotate: "rotate-2",
+  },
+  {
+    name: "Tailwind",
+    level: 3,
+    color: "bg-cyan-600 text-white",
+    rotate: "-rotate-3",
+  },
+  {
+    name: "React Router",
+    level: 4,
+    color: "bg-gray-200 text-gray-800",
+    rotate: "rotate-3",
+  },
+  {
+    name: "Sass",
+    level: 4,
+    color: "bg-pink-500 text-white",
+    rotate: "-rotate-1",
+  },
+  {
+    name: "CSS",
+    level: 5,
+    color: "bg-cyan-400 text-gray-900",
+    rotate: "rotate-1",
+  },
+  {
+    name: "Git",
+    level: 4,
+    color: "bg-gray-200 text-gray-800",
+    rotate: "rotate-0",
+  },
+  {
+    name: "HTML",
+    level: 5,
+    color: "bg-cyan-600 text-white",
+    rotate: "-rotate-2",
+  },
+  {
+    name: "Figma",
+    level: 4,
+    color: "bg-cyan-400 text-gray-900",
+    rotate: "rotate-0",
+  },
+  {
+    name: "API",
+    level: 4,
+    color: "bg-cyan-700 text-white",
+    rotate: "-rotate-3",
+  },
+  {
+    name: "Swagger",
+    level: 2,
+    color: "bg-cyan-600 text-white",
+    rotate: "rotate-1",
+  },
+];
+
+function SkillsCloud() {
+  return (
+    <>
+      {skillsData.map((skill, i) => {
+        // Taille du texte selon le niveau
+        const sizeClass =
+          skill.level === 5
+            ? "text-3xl"
+            : skill.level === 4
+              ? "text-xl"
+              : skill.level === 3
+                ? "text-lg"
+                : "text-sm";
+
+        return (
+          <span
+            key={i}
+            className={`${sizeClass} ${skill.color} ${skill.rotate} px-3 py-1 rounded-md font-semibold shadow-sm inline-block m-2`}
+          >
+            {skill.name}
+          </span>
+        );
+      })}
+    </>
+  );
+}
+
+export default SkillsCloud;
